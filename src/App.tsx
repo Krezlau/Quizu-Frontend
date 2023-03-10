1078e6e8eb7e0dde8a38db2421257ecf6157b83a
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-03-10 16:17:42 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NavigationBar from "./Components/NavigationBar";
+
+function App() {
+
+  return (
+    <>
+      <NavigationBar/>
+      <h1 className="text-3xl font-bold underline dark:text-amber-400">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
