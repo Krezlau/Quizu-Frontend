703875ce75eec1fb852072acbe32a93211113859
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-03-10 18:57:04 +0100

    search bar button

--- src/Components/Navigation/Search.tsx
+++ src/Components/Navigation/Search.tsx
@@ -1,8 +1,13 @@
 const Search = () => {
-  return <div className="form-control w-full">
+  return <div className="form-control w-full flex flex-row justify-center">
     <input type="text" placeholder="Search"
-           className="hidden input input-bordered opacity-60 w-full max-w-2xl sm:block "/>
-    <button className="btn btn-circle btn-sm ml-auto mr-0 btn-ghost sm:hidden">
+           className="hidden input input-bordered opacity-60 w-full max-w-2xl rounded-r-none sm:block "/>
+    <button className="hidden btn btn-circle opacity-60 rounded-l-none no-animation sm:block">
+        <span className="material-symbols-outlined">
+          search
+        </span>
+    </button>
+    <button className="btn btn-circle ml-auto mr-0 btn-ghost sm:hidden">
         <span className="material-symbols-outlined">
           search
         </span>
