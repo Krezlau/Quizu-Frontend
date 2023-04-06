8019d5d69fb6892890227bf6bb3735e06695a1c2
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-04-06 19:04:20 +0200

    SignupForm update

--- src/Components/Forms/SignupForm.tsx
+++ src/Components/Forms/SignupForm.tsx
@@ -10,7 +10,17 @@ const SignupForm = () => {
     <SectionHeader text={"Password"} centered={true} label={"password"}/>
     <input type="password" id="password" className="input w-full mx-auto sm:max-w-md"/>
     <SectionHeader text={"Repeat Password"} centered={true} label={"repeat-password"}/>
-    <input type="password" id="repeat-password" className="input w-full mx-auto mb-12 sm:max-w-md"/>
+    <input type="password" id="repeat-password" className="input w-full mx-auto sm:max-w-md"/>
+    <SectionHeader text={"Name"} centered={true} label={"name"}/>
+    <input type="text" id="name" className="input w-full mx-auto sm:max-w-md"/>
+    <SectionHeader text={"Surname"} centered={true} label={"surname"}/>
+    <input type="text" id="surname" className="input w-full mx-auto sm:max-w-md"/>
+    <SectionHeader text="Location" centered={true} label="location" />
+    <select className="select w-full max-w-xs mx-auto mb-12">
+      <option disabled selected>Choose a country</option>
+      <option>Poland</option>
+      <option>USA</option>
+    </select>
     <div className="mx-auto w-full sm:max-w-sm">
       <button className="btn w-full mx-auto" type="submit">Register</button>
       <div className="divider">OR</div>
