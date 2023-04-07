4cce81c71f1b6fcf013e2b0ecc74e1c760abac6b
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-04-07 15:26:10 +0200

    NotLoggedInPage, using store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import NavigationBar from "./Components/Navigation/NavigationBar";
 import PageLayout from "./Components/Pages/PageLayout";
-import React from "react";
-import {Route, Routes} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./Components/Pages/HomePage";
 import UserProfilePage from "./Components/Pages/UserProfilePage";
 import LoginPage from "./Components/Pages/LoginPage";
@@ -13,32 +13,71 @@ import ChangePasswordPage from "./Components/Pages/ChangePasswordPage";
 import CreateNewQuizPage from "./Components/Pages/CreateNewQuizPage";
 import ManageQuizPage from "./Components/Pages/ManageQuizPage";
 import QuizManageInfoPage from "./Components/Pages/QuizManageInfoPage";
+import { useSelector } from "react-redux";
+import { IRootState } from "./store";
+import { clearAuthStorage, retrieveStoredToken } from "./store/auth-actions";
+import { useDispatch } from "react-redux";
+import { authActions } from "./store/auth-slice";
+import NotLoggedInPage from "./Components/Pages/NotLoggedInPage";
 
 function App() {
+  const isLoggedIn = useSelector((state: IRootState) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const tokenData = retrieveStoredToken();
+
+    if (!tokenData) {
+      clearAuthStorage();
+      return;
+    }
+
+    dispatch(
+      authActions.login({
+        username: tokenData.username,
+        userId: tokenData.userId,
+        accessToken: tokenData.token,
+      })
+    );
+  }, [dispatch]);
 
   return (
     <>
-      <NavigationBar/>
+      <NavigationBar />
       <PageLayout>
         <Routes>
-          <Route path="/" element={<h1 className="text-3xl font-bold underline dark:text-amber-400">
-            Hello world!
-          </h1>}/>
-          <Route path="/home" element={<HomePage />}/>
-          <Route path="/user/:userId/profile" element={<UserProfilePage />}/>
-          <Route path="/user/:userId/quizzes" element={<UserQuizzesPage />}/>
-          <Route path="/user/:userId/edit" element={<EditProfilePage />}/>
-          <Route path="/quizzes/:quizId/details" element={<QuizDetailsPage />}/>
-          <Route path="/quizzes/:quizId/manage-info" element={<QuizManageInfoPage />} />
-          <Route path="/quizzes/:quizId/manage" element={<ManageQuizPage />}/>
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/signup" element={<SignupPage />}/>
-          <Route path="/change-password" element={<ChangePasswordPage />}/>
-          <Route path="/new-quiz" element={<CreateNewQuizPage />}/>
+          <Route
+            path="/"
+            element={
+              <h1 className="text-3xl font-bold underline dark:text-amber-400">
+                Hello world!
+              </h1>
+            }
+          />
+          <Route path="/home" element={isLoggedIn ? <HomePage /> : <Navigate to="/about"/>} />
+          <Route path="/user/:userId/profile" element={<UserProfilePage />} />
+          <Route path="/user/:userId/quizzes" element={<UserQuizzesPage />} />
+          <Route path="/user/:userId/edit" element={<EditProfilePage />} />
+          <Route
+            path="/quizzes/:quizId/details"
+            element={<QuizDetailsPage />}
+          />
+          <Route
+            path="/quizzes/:quizId/manage-info"
+            element={<QuizManageInfoPage />}
+          />
+          <Route path="/quizzes/:quizId/manage" element={<ManageQuizPage />} />
+          <Route path="/login" element={isLoggedIn ? <Navigate to="/home"/> : <LoginPage />} />
+          <Route path="/signup" element={isLoggedIn ? <Navigate to="/home"/> : <SignupPage />} />
+          <Route
+            path="/change-password"
+            element={isLoggedIn ? <ChangePasswordPage /> : <NotLoggedInPage />}
+          />
+          <Route path="/new-quiz" element={isLoggedIn ? <CreateNewQuizPage /> : <NotLoggedInPage/>} />
         </Routes>
       </PageLayout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
