03fcb60d80ec33044b191a0b32df04dc94bc3355
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-05-20 14:40:45 +0200

    playfetchpage

--- src/Components/Pages/PlayFetchPage.tsx
+++ src/Components/Pages/PlayFetchPage.tsx
@@ -2,6 +2,14 @@ import LoadingSpinner from "../UI/LoadingSpinner";
 import PageHeader from "../UI/PageHeader";
 
 const PlayFetchPage = () => {
+  // fetch questions (need new endpoint here) with settings
+  // dispatch state change 
+  // redirect to /play?quizId={string}...
+  //
+  // need endpoints:
+  // fetch questions(without answers) and settings
+  // check if answer is correct (or submit answer?) => return score gained??
+  // save score
   return <>
     <PageHeader text="Starting play mode..."/>
     <LoadingSpinner />
