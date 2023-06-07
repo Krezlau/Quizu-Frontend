f17fa2e14da4b7a27339d435f5c528384a901ec9
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-06-07 17:57:40 +0200

    stats - responsive

--- src/Components/Quizzes/QuizStatsCard.tsx
+++ src/Components/Quizzes/QuizStatsCard.tsx
@@ -1,3 +1,4 @@
+import { ResponsiveContainer } from "recharts";
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
 import IQuizStats from "../../types/IQuizStats";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -8,10 +9,15 @@ const QuizStatsCard: React.FC<{
 }> = (props) => {
   return (
     <div className="card bg-neutral p-4 text-xl">
+      {!props.isLoading && props.stats && props.stats.totalPlays === 0 && (
+        <p className="text-center text-2xl my-2">
+          This quiz has not been played yet. Be the first!
+        </p>
+      )}
       {props.isLoading && <LoadingSpinner size="xl" center={true} />}
       {!props.isLoading && props.stats && (
         <>
-          <div className="flex flex-row justify-between my-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-0 lg:grid-cols-3 my-6">
             <div className="flex flex-col mx-auto text-center">
               <span className="text-2xl font-bold">TOTAL PLAYS</span>
               <span className="text-6xl font-extrabold text-primary">
@@ -21,7 +27,7 @@ const QuizStatsCard: React.FC<{
             <div className="flex flex-col mx-auto text-center">
               <span className="text-2xl font-bold">AVERAGE SCORE</span>
               <span className="text-6xl font-extrabold text-primary">
-                {props.stats.averageScore}
+                {props.stats.averageScore.toFixed(2)}
               </span>
             </div>
             <div className="flex flex-col mx-auto text-center">
@@ -30,23 +36,35 @@ const QuizStatsCard: React.FC<{
                 {props.stats.averageTimeS.toFixed(2)} s
               </span>
             </div>
-          </div>
-          <div className="flex flex-row justify-between my-6">
-            <div className="flex flex-col mx-auto text-center my-auto">
-              <span className="text-4xl font-bold">TOP SCORE</span>
-              <span className="text-8xl font-extrabold text-primary">
-                {props.stats.topScore}
+            <div className="lg:hidden flex flex-col mx-auto text-center my-auto">
+              <span className="text-2xl font-bold">TOP SCORE</span>
+              <span className="text-6xl font-extrabold text-primary">
+                {props.stats.topScore.toFixed(0)}
               </span>
             </div>
-            <div>
-              <p className="text-center mb-4">Score chart</p>
-              <BarChart data={props.stats.plotPoints} width={730} height={250}>
-                <XAxis dataKey="score" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="count" fill="#8884d8" />
-              </BarChart>
+          </div>
+          <div className="flex flex-col lg:flex-row justify-between my-6">
+            <div className="hidden lg:flex flex-col mx-auto text-center my-auto">
+              <span className="text-4xl font-bold">TOP SCORE</span>
+              <span className="text-8xl font-extrabold text-primary">
+                {props.stats.topScore.toFixed(0)}
+              </span>
             </div>
+            {props.stats.totalPlays > 0 && (
+              <div className="w-full lg:w-3/5 p-0">
+                <p className="text-center text-2xl font-bold mb-4">SCORE CHART</p>
+                <ResponsiveContainer width="100%" height={300}>
+                  <BarChart
+                    data={props.stats.plotPoints}
+                    margin={{top:0,right:0,bottom:0,left:0}}
+                  >
+                    <XAxis dataKey="score" />
+                    <YAxis allowDecimals={false} width={40}/>
+                    <Bar dataKey="count" fill="#8884d8" />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            )}
           </div>
         </>
       )}
