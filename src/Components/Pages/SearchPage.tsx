4cb3274299df8baac9ec7a594ca87df1a563675b
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-09-29 19:27:29 +0200

    search page fetching

--- src/Components/Pages/SearchPage.tsx
+++ src/Components/Pages/SearchPage.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import useHttp from "../../hooks/useHttp";
 import { IRootState } from "../../store";
 import { searchActions } from "../../store/search-slice";
+import IPageResponse from "../../types/IPageResponse";
+import IQuiz from "../../types/IQuiz";
 import QuizList from "../Quizzes/QuizList";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import SectionHeader from "../UI/SectionHeader";
 
 const SearchPage = () => {
   const searchText = useSelector((state: IRootState) => state.search.text);
   const dispatch = useDispatch();
+  const { isLoading, searchQuiz } = useHttp();
+  const [result, setResult] = useState<IPageResponse<IQuiz>>();
 
   const searchChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(searchActions.change({ text: event.target.value }));
   };
+
+  useEffect(() => {
+    if (searchText.length > 0) {
+      searchQuiz("123", 0, 10).then((r) => setResult(r));
+    }
+  }, [searchText]);
 
   return (
     <>
@@ -27,6 +40,17 @@ const SearchPage = () => {
         </button>
       </div>
       <SectionHeader text="Search Results" centered={true} />
+      {!result && <LoadingSpinner center={true} />}
+      {result && (
+        <QuizList
+          quizzes={result && result.queryResult ? result.queryResult : []}
+          isLoading={isLoading ? isLoading : false}
+          loadMore={() => {
+            searchQuiz("123", 0, 10).then((r) => setResult(r));
+          }}
+          isAllLoaded={result?.pageCount <= result?.pageNumber + 1}
+        />
+      )}
     </>
   );
 };
